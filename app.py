18cf701345ea5f2d88475ea81db6323e7fafc533
Ashok Nagaraj <nashok@cisco.com>
2023-07-10 11:51:02 +0530

    Add basic logging

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import time
 
@@ -8,25 +9,38 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 api = FlaskAPI(__name__)
 FlaskInstrumentor().instrument_app(api)
 
+logFormatter = logging.Formatter(fmt=' %(asctime)s :: %(name)s :: %(levelname)s :: %(message)s')
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.INFO)
+consoleHandler.setFormatter(logFormatter)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(consoleHandler)
+
 @api.route('/')
 def root_path():
     return jsonify({"message": "root path of the app"}), status.HTTP_200_OK
 
 def _sleep():
+    logger.info("sleeping ...")
     time.sleep(1)
 
 def _roll_dice():
+    logger.info("rolling dice ...")
     return random.randint(0, 9)
 
 @api.route('/roll')
 def roll_path():
     _sleep()
     r = _roll_dice()
+    logger.warning(f"got {r} on the roll")
     if r in range(1, 6):
         return jsonify({"output": r}), status.HTTP_200_OK
     elif r > 6:
+        logger.error(f"value:{r} out of range")
         return jsonify({"error": f"output:{r} beyond range(1-6)"}), status.HTTP_501_NOT_IMPLEMENTED
     else:
+        logger.critical(f"value:{r} unexpected")
         return jsonify({"error": f"output=0"}), status.HTTP_500_INTERNAL_SERVER_ERROR
 
 if __name__ == '__main__':
